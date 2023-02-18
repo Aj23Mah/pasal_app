b90d16845937acffb58089b92179ad10409acd5d
ajayMaharjan <ajaymaharjan758@gmail.com>
2023-02-18 13:58:16 +0545

    create the spinner component

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -30,7 +30,7 @@ const Navbar = () => {
   }
 
   return (
-    <div className="bg-white border-b shadow-5m sticky top-0 z-50">
+    <div className="bg-white border-b shadow-5m sticky top-0 z-40">
       <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
         <div>
           <img
